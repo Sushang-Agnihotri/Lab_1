29be6fcdf39d617f85a490f6f550ba3e4fe1054a
Sushang-Agnihotri <sushangagnihotri@gmail.com>
2023-05-25 09:23:10 -0400

    First commit

---
+++ Lab_1.py
@@ -0,0 +1,21 @@
+# Variable Declaration
+name = "John"
+age = 25
+
+# Conditional Statements
+if age >= 18:
+    print("Welcome,", name + "! You are eligible to vote.")
+else:
+    print("Sorry,", name + ". You are not eligible to vote yet.")
+
+# Loop Example
+for i in range(1, 6):
+    print("Count:", i)
+
+# Function Definition
+def calculate_sum(a, b):
+    return a + b
+
+# Function Call
+result = calculate_sum(3, 4)
+print("Sum:", result)
